bc9ea21d18a633cca65c22ff1e7dd4639f6ed8fd
Sohel <sohel.techin@gmail.com>
2024-09-11 18:14:12 +0530

    Adding Navbar to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css';
-import { useState } from 'react';
+import Navbar from './components/Navbar';
+import News from './components/News';
+
 
 function App() {
 
   return (
 	<>
+    	<Navbar/>
+		<News/>
+    
 	</>
   )
 }
---
+++ src/components/NewsItem.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default NewsItem

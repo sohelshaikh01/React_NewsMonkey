7e8fb6496c2cd43081df60843a1b9c039d6b755b
Sohel <sohel.techin@gmail.com>
2024-09-12 20:09:52 +0530

    Develop Proper Button

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -14,10 +14,10 @@ export class NewsItem extends Component {
 
         <div className="card-body flex flex-col justify-center gap-2 p-4">
           <h5 className="card-title text-xl font-bold">{title.slice(0, 50)}...</h5>
-          <p className="card-text">{description.slice(0, 120)}...</p>
-          <p>{source} </p>
-          <small className='text-muted'>By {author?author:'unknown'} on {new Date(date).toGMTString()}</small>
-          <a href={url}> Read More</a>
+          <p className="card-text ">{description.slice(0, 120)}...</p>
+          <p className="font-semibold">{source} </p>
+          <small className='text-muted text-sm leading-5'>By {author?author:'unknown'} on {new Date(date).toGMTString()}</small>
+          <a href={url} className="bg-blue-500 inline-block px-4 py-1 text-white mt-2 "> Read More</a>
         </div>
       </div>
     )
